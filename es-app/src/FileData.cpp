a82d3cc8e81b1abbd483e09fbe004d95e979d4e3
opa <opa@ken16.net>
2024-07-14 21:05:21 +0900

    window->normalizeNextUpdate()

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -314,8 +314,8 @@ void FileData::launchGame(Window* window)
 	window->init();
 	InputManager::getInstance()->init();
 	VolumeControl::getInstance()->init();
-	window->normalizeNextUpdate();
 #endif
+	window->normalizeNextUpdate();
 
 	//update number of times the game has been launched
 
