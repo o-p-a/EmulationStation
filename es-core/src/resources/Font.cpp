72146ef6fd0a14eae5d0617076361dc1d35cc2da
opa <opa@ken16.net>
2024-07-18 19:56:39 +0900

    bindTexture

--- es-core/src/resources/Font.cpp
+++ es-core/src/resources/Font.cpp
@@ -408,12 +408,13 @@ void Font::renderTextCache(TextCache* cache)
 LOG(LogInfo) << "Font::renderTextCache start"; Log::flush();
 	for(auto it = cache->vertexLists.cbegin(); it != cache->vertexLists.cend(); it++)
 	{
-LOG(LogInfo) << "textureIdPtr " << (void*)(*it->textureIdPtr); Log::flush();
 		assert(*it->textureIdPtr != 0);
 
 		auto vertexList = *it;
 
+LOG(LogInfo) << "bindTexture"; Log::flush();
 		Renderer::bindTexture(*it->textureIdPtr);
+LOG(LogInfo) << "drawTriangleStrips"; Log::flush();
 		Renderer::drawTriangleStrips(&it->verts[0], (int)it->verts.size());
 	}
 LOG(LogInfo) << "Font::renderTextCache end"; Log::flush();
