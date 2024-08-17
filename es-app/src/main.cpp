ec5ecfb86707d00f4bc3bff9effe81a947ff906c
opa <opa@ken16.net>
2024-08-17 17:53:11 +0900

    fix

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -1,22 +1,6 @@
 //EmulationStation, a graphical front-end for ROM browsing. Created by Alec "Aloshi" Lofquist.
 //http://www.aloshi.com
 
-#include "guis/GuiDetectDevice.h"
-#include "guis/GuiMsgBox.h"
-#include "utils/FileSystemUtil.h"
-#include "utils/ProfilingUtil.h"
-#include "views/ViewController.h"
-#include "CollectionSystemManager.h"
-#include "EmulationStation.h"
-#include "InputManager.h"
-#include "Log.h"
-#include "MameNames.h"
-#include "platform.h"
-#include "PowerSaver.h"
-#include "ScraperCmdLine.h"
-#include "Settings.h"
-#include "SystemData.h"
-#include "SystemScreenSaver.h"
 #include <SDL_events.h>
 #include <SDL_main.h>
 #include <SDL_timer.h>
