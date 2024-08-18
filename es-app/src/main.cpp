348a5a309f9cc490e98722c552380d15862ba9c3
opa <opa@ken16.net>
2024-08-18 14:50:07 +0900

    GL_QUADS

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -173,6 +173,13 @@ int main(int argc, char* argv[])
 			}
 		}
 
+        glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(-0.5f, 0.5f);
+        glEnd();
+
 		swapBuffers();
 	}
 
